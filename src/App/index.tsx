e7a1b2ac34f01100fc4bbbb13b511c1901209152
sanjaybora15 <sanjaybora15@gmail.com>
2021-07-01 17:16:30 +0530

    updated

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import Counter from '../components/Counter';
 import './index.scss';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const App = () => {
     const [count, setCount] = useState<number>(5);
-
     const onChangeHandler = (fieldName: string, value: number) => {
         setCount(value);
+    }
+    useEffect(() => {
+      let timer = setTimeout(() => {
+        saveCounterData()
+      }, 300);
+      return () => {
+        clearTimeout(timer);
+      }
+    }, [count])
+
+    const saveCounterData = () => {
+      fetch("https://interview-8e4c5-default-rtdb.firebaseio.com/front-end.json", {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({count})
+      }).then(res => {
+        console.log(res)
+      })
     }
 
   return (
